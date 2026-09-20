2d260b4232326067a77eed3e16adac07d8cdf31d
Marcos Daniel <marcosnumber11@gmail.com>
2026-09-20 20:55:44 -0300

    feat: display movie catalogue and screening times

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,8 +1,11 @@
+from django.contrib import admin
 from django.urls import path
 
-from .views import cart_detail, home
+from .views import cart_detail, home, movie_detail
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('filmes/<slug:slug>/', movie_detail, name='movie-detail'),
     path('carrinho/', cart_detail, name='cart-detail'),
 ]
